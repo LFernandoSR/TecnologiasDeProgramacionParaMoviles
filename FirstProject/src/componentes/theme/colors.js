ea469e20c2bea5d53e19cb02a2253cd18744258e
LFernandoSR <fernandosanchezruiz7@gmail.com>
2023-08-31 22:38:34 -0600

    inicio

---
+++ FirstProject/src/componentes/theme/colors.js
@@ -0,0 +1,12 @@
+export const THEME = {
+    COLORS: {
+        DARK_BLUE: '#202b44',
+        WHITE: 'white',
+        GREEN: '#abd638',
+        GRAY: 'silver',
+        GREEN_LIGTH: '#63cb77',
+        GREEN_DARK: '#176b5b',
+    }
+
+
+}
