103523ab4020159ce7a22ebf3192cd9ce68831c6
LFernandoSR <fernandosanchezruiz7@gmail.com>
2023-09-21 07:06:05 -0600

    tarea3

---
+++ FirstProject/src/componentes/TodoInput.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { StyleSheet, TextInput, View } from "react-native";
+
+const TodoInput = ({value, onChangeText}) => {
+    return (
+        <TextInput 
+            style={styles.container}
+            value={value}
+            onChangeText={onChangeText}
+            placeholder="Escrbe aquí tu tarea"
+        />
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        borderWidth:1, 
+        borderColor: 'white', 
+        flex: 1,
+        borderRadius: 10,
+        color: 'white',
+        padding: 10,
+        height: 40
+    }
+})
+
+export default TodoInput
